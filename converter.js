1400504e68a88c465390a034406ea8326cc276ce
yumemiku <danielat97@hotmail.com>
2023-02-09 13:18:20 +0000

    converted from document.write to text area

--- converter.js
+++ converter.js
@@ -1,5 +1,7 @@
 //Change the below number to set your temperature in kelvin.
 kelvin = prompt("Enter value");
+const resultstext = document.getElementById("results")
+console.log(resultstext)
 //Celsius is always (constantly) 273 degrees less than Kelvin
 const celsius = kelvin - 273;
 //Below is the equation for converting celsius to fahrenheit
@@ -7,14 +9,14 @@ let fahrenheit = celsius * (9/5) + 32;
 //We will need to round down the temperature so that we don't get a decimal
 fahrenheit = Math.floor(fahrenheit);
 //This will be the first line
-document.write(`If the temperature is ${kelvin} degrees Kelvin, then:`+ "<br>")
+resultstext.innerHTML += `If the temperature is ${kelvin} degrees Kelvin, then:<br>`;
 //Temperature in Fahrenheit
-document.write(`The temperature is ${fahrenheit} degrees Fahrenheit.`+ "<br>");
+resultstext.innerHTML +=`The temperature is ${fahrenheit} degrees Fahrenheit.`+ "<br>";
 //Temperature in Celsius
-document.write(`The temperature is ${celsius} degrees Celsius,`+ "<br>");
+resultstext.innerHTML +=`The temperature is ${celsius} degrees Celsius,`+ "<br>";
 //Extra: converting to Newton
 let newton = celsius * (33/100);
 //We will need to round down the temperature so that we don't get a decimal
 newton = Math.floor(newton);
 //Temperature in Newton
-document.write(`The temperature is ${newton} degrees Newton.`+ "<br>")
+resultstext.innerHTML +=`The temperature is ${newton} degrees Newton.`+ "<br>";
